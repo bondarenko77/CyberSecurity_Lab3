d2bd4a01244091a0d8a30d5526519a20a6b75402
Yana <bond.tyan@gmail.com>
2026-05-05 23:27:26 +0300

    Додано структуру Лабораторної №3: robots, htaccess та капча

---
+++ admin_panel.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Panel - CyberGuard</title>
+    <!-- Підключаємо скрипт Google -->
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
+<body>
+    <h1>Адмін-панель (Конфіденційно)</h1>
+    <p>Цей розділ захищено від автоматизованого доступу ботів.</p>
+    
+    <form action="?" method="POST">
+        <!-- Вставляємо віджет із твоім ключем -->
+        <div class="g-recaptcha" data-sitekey="6LcFEdssAAAAAJcm2ebC-_l8LsmE03872Lyf6eaZ"></div>
+        <br/>
+        <input type="submit" value="Увійти">
+    </form>
+</body>
+</html>
